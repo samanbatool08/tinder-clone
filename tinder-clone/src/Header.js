239a13f30412217699972b03ef7d439c45d7ca73
Your Name <samanbatool08@gmail.com>
2020-08-06 17:07:59 -0400

    header component flex

--- tinder-clone/src/Header.js
+++ tinder-clone/src/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './Header.css';
 import PersonIcon from '@material-ui/icons/Person';
 import ForumIcon from '@material-ui/icons/Forum';
 
 function Header() {
     return (
-        <div>
+        // BEM 
+        <div className='header'>
             <PersonIcon />
             <h1>I am a Header</h1>
             <ForumIcon />
