c999770f732ccde265ee491fa0aa95657b1c8f5e
Your Name <samanbatool08@gmail.com>
2020-08-06 19:38:11 -0400

    successfully connected to database

--- tinder-clone/src/TinderCards.js
+++ tinder-clone/src/TinderCards.js
@@ -1,20 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import database from './firebase';
 import TinderCard from 'react-tinder-card'
 import './TinderCards.css';
 
 function TinderCards() {
 
     // const people = []
-    const [people, setPeople] = React.useState([
-        {
-            name: 'steve jobs',
-            url: 'https://www.biography.com/.image/t_share/MTY2MzU3OTcxMTUwODQxNTM1/steve-jobs--david-paul-morrisbloomberg-via-getty-images.jpg'
-        }, 
-        {
-            name: 'mark zuckerberg',
-            url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg/220px-Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg'
-        }
-    ]);
+    const [people, setPeople] = useState([]);
+
+    // runs based on a conditional
+    useEffect(() => {
+        database.collection('people').onSnapshot(snapshot => (
+            setPeople(snapshot.docs.map(doc => doc.data()))
+        ))
+
+        // keeping the dependency blank will run the code only once when the page loads 
+    }, [])
 
     return(
         <div>
