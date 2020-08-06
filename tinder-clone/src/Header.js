6914216b70afb089e7fcb9d6a853cfbcb98f6174
Your Name <samanbatool08@gmail.com>
2020-08-06 17:04:48 -0400

    import all icons from material-ui

--- tinder-clone/src/Header.js
+++ tinder-clone/src/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PersonIcon from '@material-ui/icons/Person';
+import ForumIcon from '@material-ui/icons/Forum';
 
 function Header() {
     return (
         <div>
             <PersonIcon />
-            <h1>header</h1>
+            <h1>I am a Header</h1>
+            <ForumIcon />
         </div>
     )
 }
