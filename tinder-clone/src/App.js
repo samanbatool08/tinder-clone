3ea1840032e49b91bf9d765fe47eee6dda25a0b6
Your Name <samanbatool08@gmail.com>
2020-08-06 18:02:35 -0400

    dynamic routing

--- tinder-clone/src/App.js
+++ tinder-clone/src/App.js
@@ -1,22 +1,36 @@
 import React from 'react';
 import Header from './Header';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    <h1>let's build a tinder clone application</h1>
-
-    {/* Header */}
     <Header />
-    {/* Tinder Cards - profile pic + name */}
-    {/* Buttons below card */}
+    <Router>
+      <Switch>
+      <Route path='/chat'>
+          <h1>I am the chatpage</h1>
+        </Route>
 
-    {/* Chats screen list */}
-    {/* Individual chat window */}
-
+        <Route path='/'>
+          <h1>I am homepage</h1>
+        </Route>
+      </Switch>
+    </Router>
     </div>
   );
 }
 
 export default App;
+
+
+
+// component hierarchy/structure
+{/* Header */}
+
+{/* Tinder Cards - profile pic + name */}
+{/* Buttons below card */}
+
+{/* Chats screen list */}
+{/* Individual chat window */}
