0eae43eced56c0bcc90365f7378d3b6fdcc8cf58
Your Name <samanbatool08@gmail.com>
2020-08-06 22:32:48 -0400

    icon styling

--- tinder-clone/src/SwipeButtons.js
+++ tinder-clone/src/SwipeButtons.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-
+import IconButton from '@material-ui/core/IconButton';
 import ReplayIcon from '@material-ui/icons/Replay'
 import CloseIcon from '@material-ui/icons/Close'
 import StarRateIcon from '@material-ui/icons/Replay'
@@ -12,12 +12,25 @@ import './SwipeButtons.css'
 function SwipeButtons() {
     return(
         <div className='swipeButtons'>
+            <IconButton className='swipeButtons__repeat'>
+                <ReplayIcon fontSize='large' />
+            </IconButton>
 
-            <ReplayIcon fontSize='large' />
-            <CloseIcon fontSize='large' />
-            <StarRateIcon fontSize='large' />
-            <FavoriteIcon fontSize='large' />
-            <FlashOnIcon fontSize='large' />
+            <IconButton className='swipeButtons__left'>
+                <CloseIcon fontSize='large' />
+            </IconButton>
+
+            <IconButton className='swipeButtons__star'>
+                <StarRateIcon fontSize='large' />
+            </IconButton>
+
+            <IconButton className='swipeButtons__right'>
+                <FavoriteIcon fontSize='large' />
+            </IconButton>
+
+            <IconButton className='swipeButtons__lightning'>
+                <FlashOnIcon fontSize='large' />
+            </IconButton>
 
         </div>
     )
