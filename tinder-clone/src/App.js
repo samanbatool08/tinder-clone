495d1f71d4776d3109ba518d771e40d349af416e
Your Name <samanbatool08@gmail.com>
2020-08-07 12:56:16 -0400

    usehistory - backbutton routing

--- tinder-clone/src/App.js
+++ tinder-clone/src/App.js
@@ -9,7 +9,6 @@ function App() {
   return (
     <div className="App">
     <Router>
-      {/* <Header /> */}
       <Switch>
       <Route path='/chats'>
           <Header backButton='/' />
--- tinder-clone/src/Header.js
+++ tinder-clone/src/Header.js
@@ -3,18 +3,29 @@ import './Header.css';
 import PersonIcon from '@material-ui/icons/Person';
 import ForumIcon from '@material-ui/icons/Forum';
 import IconButton from '@material-ui/core/IconButton';
-import { Link } from 'react-router-dom';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import { Link, useHistory } from 'react-router-dom';
+import { Icon } from '@material-ui/core';
 
-const Header = () => {
+const Header = ({ backButton }) => {
 // function Header() {
+
+const history = useHistory();
 
     return (
         // BEM 
         <div className='header'>
+            {backButton ? (
+                <IconButton onClick={() => history.replace(backButton)}>
+                    <ArrowBackIosIcon fontSize='large' className='header__' />
+                </IconButton>
+            ) : (
+
             <IconButton>
                <PersonIcon className='header__icon' fontSize='large'/>
             </IconButton>
             
+            )}
             <Link to='/'>
             <img 
                 className="header__logo"
