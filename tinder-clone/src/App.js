7761d437d9c52391cbf81bc23b3b268e4dfa5f6c
samanbatool08 <56152644+samanbatool08@users.noreply.github.com>
2020-08-06 21:59:25 -0400

    Revert "Lower screen buttons"

--- tinder-clone/src/App.js
+++ tinder-clone/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from './Header';
 import TinderCards from './TinderCards';
-import SwipeButtons from './SwipeButtons';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
@@ -17,8 +16,6 @@ function App() {
 
         <Route path='/'>
           <TinderCards />
-          <SwipeButtons />
-
         </Route>
       </Switch>
     </Router>
