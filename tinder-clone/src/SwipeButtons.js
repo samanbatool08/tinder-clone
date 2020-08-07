f704e3dac0fcf870fe34bd7fc6598d3780d2a0e9
Your Name <samanbatool08@gmail.com>
2020-08-06 21:16:48 -0400

    import icons material ui

--- tinder-clone/src/SwipeButtons.js
+++ tinder-clone/src/SwipeButtons.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import ReplayIcon from '@material-ui/icons/Replay';
+import CloseIcon from '@material-ui/icons/Close';
+import StarRateIcon from '@material-ui/icons/StarRate';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import FlashOnIcon from '@material-ui/icons/FlashOn';
+import './SwipeButtons.css';
 
 function SwipeButtons() {
     return (
-        <div>
-            <h2>Buttons go here</h2>
+        <div className='swipeButtons'>
+            <ReplayIcon fontSize='large' />
+            <CloseIcon fontSize='large' />
+            <StarRateIcon fontSize='large' />
+            <FavoriteIcon fontSize='large' />
+            <FlashOnIcon fontSize='large' />
+
         </div>
     )
 }
