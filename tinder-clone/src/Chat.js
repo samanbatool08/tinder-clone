48c19fcd0cf81449b6db25ecef68090ba45366ee
Your Name <samanbatool08@gmail.com>
2020-08-07 13:29:23 -0400

    individual chat compoent

---
+++ tinder-clone/src/Chat.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Chat({ name, message, timestamp, profilePic }) {
+    return (
+        <div className='chat'>
+            
+
+        </div>
+    )
+}
+
+export default Chat;
